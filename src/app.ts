88c143d7ad16f1d0f6cf3a3a676d52f599de6fab
Akinpelu Abiodun <59386529+Matthabbey@users.noreply.github.com>
2023-09-23 01:11:54 +0100

    first commit

---
+++ src/app.ts
@@ -0,0 +1,39 @@
+import { Request, Response, NextFunction } from "express";
+import express from "express";
+import path from "path";
+import cookieParser from "cookie-parser";
+import logger from "morgan";
+import usersRouter from "./routes/users";
+
+const app = express();
+
+// view engine setup
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "jade");
+
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use("/users", usersRouter);
+
+// error handler
+app.use(function (err: Error, req: Request, res: Response, next: NextFunction) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
+
+  // render the error page
+  res.status(500);
+  res.render("error");
+});
+
+const port = 4000;
+
+app.listen(port, () => {
+  console.log(`Server runing on http://${port}`);
+});
+
+export default app;
